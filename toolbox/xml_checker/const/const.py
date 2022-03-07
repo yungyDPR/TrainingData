0e397c55e35a53fae7b896a73633b2ae2aed34df
hugo scheithauer <hugoscheithauer@outlook.fr>
2022-03-07 18:17:15 +0100

    Adding an XML validation tool

---
+++ toolbox/xml_checker/const/const.py
@@ -0,0 +1,13 @@
+"""
+Constants variables for validating trainign data without a XSD file.
+"""
+
+GROBID_MODELS = ['segmentation']
+
+TEI_TAGS = ['tei', 'teiHeader', 'fileDesc', 'text']
+
+SEGMENTATION_TAGS = ['text', 'cover', 'title', 'publisher', 'summary',
+                                'biography', 'advertising', 'toc', 'tof',
+                                'preface', 'dedication', 'unit', 'reference',
+                                'annex', 'index', 'back', 'other']
+
---
+++ toolbox/xml_checker/grobid_xml_checker.py
@@ -0,0 +1,52 @@
+"""
+Script for validating training data, either using a XSD file or python.
+"""
+from const import const
+
+import os
+
+from lxml import etree
+
+rootdir = '../../datasets'
+
+
+def validate_with_xsd(rootdir: str):
+    for subdir, dirs, files in os.walk(rootdir):
+        for model in const.GROBID_MODELS:
+            if subdir.endswith(model):
+                for subdir, dirs, files in os.walk(rootdir):
+                    if subdir.endswith('tei'):
+                        for file in files:
+                            filename = os.path.join(subdir, file)
+                            print(filename)
+                            with open(filename, 'r', encoding='utf8') as fh:
+                                file = fh.read().replace('<fileDesc xml:id="0"/>', '<fileDesc/>')
+                                try:
+                                    schema_root = etree.parse('./xsd/highLvlSeg.xsd')
+                                    schema = etree.XMLSchema(schema_root)
+                                    xml_parser = etree.XMLParser(schema=schema)
+                                    tree = etree.fromstring(file, parser=xml_parser)
+                                except etree.XMLSyntaxError as e:
+                                    print(e)
+
+# TODO: Keep a log after validation
+
+
+def validate_with_python(rootdir: str):
+    """
+    Does not for for now.
+    """
+    for subdir, dirs, files in os.walk(rootdir):
+        for model in const.GROBID_MODELS:
+            if subdir.endswith(model):
+                for subdir, dirs, files in os.walk(rootdir):
+                    if subdir.endswith('tei'):
+                        for file in files:
+                            filename = os.path.join(subdir, file)
+                            print(filename)
+                            with open(filename, 'r', encoding='utf8') as fh:
+                                file = fh.read().replace('<fileDesc xml:id="0"/>', '<fileDesc/>')
+                                clean_tree = etree.parse(file)
+                                for tag in clean_tree.find('text').iter():
+                                        if tag.tag not in const.SEGMENTATION_TAGS:
+                                            print('erreur')
