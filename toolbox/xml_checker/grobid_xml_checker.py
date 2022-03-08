b21fe0099e0f7aa6d68fc43d87293bf7744d5964
hugo scheithauer <hugoscheithauer@outlook.fr>
2022-03-08 13:17:53 +0100

    updated

--- toolbox/xml_checker/grobid_xml_checker.py
+++ toolbox/xml_checker/grobid_xml_checker.py
@@ -28,25 +28,29 @@ def validate_with_python(rootdir: str):
                                             print('erreur')
 
 
-def validate_with_xsd(rootdir: str):
+def validate_with_xsd(rootdir: str, xsddir: str):
+    xsd = str()
     for subdir, dirs, files in os.walk(rootdir):
         for model in const.GROBID_MODELS:
             if subdir.endswith(model):
-                for subdir, dirs, files in os.walk(rootdir):
-                    if subdir.endswith('tei'):
-                        for file in files:
-                            filename = os.path.join(subdir, file)
-                            print(filename)
-                            with open(filename, 'r', encoding='utf8') as fh:
-                                file = fh.read().replace('<fileDesc xml:id="0"/>', '<fileDesc/>')
-                                try:
-                                    schema_root = etree.parse('./xsd/highLvlSeg.xsd')
-                                    schema = etree.XMLSchema(schema_root)
-                                    xml_parser = etree.XMLParser(schema=schema)
-                                    tree = etree.fromstring(file, parser=xml_parser)
-                                except etree.XMLSyntaxError as e:
-                                    print(e)
+                for subdir, dirs, files in os.walk(xsddir):
+                    for file in files:
+                        if file.split('.')[0] == model:
+                            xsd = os.path.join(subdir, file)
+                    for subdir, dirs, files in os.walk(rootdir):
+                        if subdir.endswith('tei'):
+                            for file in files:
+                                filename = os.path.join(subdir, file)
+                                print(filename)
+                                with open(filename, 'r', encoding='utf8') as fh:
+                                    file = fh.read().replace('<fileDesc xml:id="0"/>', '<fileDesc/>')
+                                    try:
+                                        schema_root = etree.parse(xsd)
+                                        schema = etree.XMLSchema(schema_root)
+                                        xml_parser = etree.XMLParser(schema=schema)
+                                        tree = etree.fromstring(file, parser=xml_parser)
+                                    except etree.XMLSyntaxError as e:
+                                        print(e)
 
 # TODO: Keep a log after validation
 # TODO: Iteration through GROBID models *AND* their respective XSD
-
